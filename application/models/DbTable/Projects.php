06b354f0c868986bd131f3841a159f0e1dfdd25f
Chris Tankersley <dragonmantank@gmail.com>
2009-03-04 03:24:53 +0000

    Clearing out old files to start project fresh

--- application/models/DbTable/Projects.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class DbTable_Projects extends Zend_Db_Table_Abstract
-{
-	protected $_name = 'Projects';
-}
--- application/models/DbTable/Tasks.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class DbTable_Tasks extends Zend_Db_Table_Abstract
-{
-	protected $_name = 'Tasks';
-}
