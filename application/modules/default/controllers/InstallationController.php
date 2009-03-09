8943979f0878bed9c9d0ec8f14b772bd462e36cc
Chris Tankersley <dragonmantank@gmail.com>
2009-03-09 03:56:13 +0000

    Adde signoffs for the rest of the sections

---
+++ application/modules/default/controllers/InstallationController.php
@@ -0,0 +1,20 @@
+<?php
+
+class InstallationController extends Zend_Controller_Action
+{
+	public function indexAction()
+	{
+		$projectId	= $_SESSION['projectId'];
+
+		$projects	= new Projects();
+		$project	= $projects->find($projectId)->current();
+		
+		$this->view->projectName	= $project->name;
+		$this->view->projectId		= $projectId;
+	}
+	
+	public function init()
+	{
+		$this->_helper->layout->setLayout('project-layout');
+	}
+}
