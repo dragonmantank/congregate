8e6f58c25520cc533174bf2ac4e6fedf8439743f
Chris Tankersley <dragonmantank@gmail.com>
2009-03-18 20:05:19 +0000

    Projects now have an auther, dateCreated, dateApproved, and dateDeclined in the database Projects that are added now record the author via the Zend_Auth identity

--- application/models/Projects.php
+++ application/models/Projects.php
@@ -3,4 +3,12 @@
 class Projects extends Zend_Db_Table_Abstract
 {
 	protected $_name	= 'p_Projects';
+
+	public function add(array $data)
+	{
+		$identity		= Zend_Auth::getInstance()->getIdentity();
+		$data['author']	= $identity->id;
+
+		$this->insert($data);
+	}
 }
--- application/modules/default/controllers/ProjectsController.php
+++ application/modules/default/controllers/ProjectsController.php
@@ -6,35 +6,35 @@ class ProjectsController extends Zend_Controller_Action
 	{
 		$this->_helper->layout->setLayout('project-layout');
 	}
-	
+
 	public function addAction()
 	{
 		$this->_helper->layout->disableLayout();
 		$this->_helper->viewRenderer->setNoRender(true);
-		
+
 		if($_POST) {
 			$projectName	= $this->_request->getParam('projectName');
 			$description	= $this->_request->getParam('projectDescription');
-		
+
 			try {
 				$projects	= new Projects();
-				$projects->insert(array('name' => $projectName, 'description' => $description));
+				$projects->add(array('name' => $projectName, 'description' => $description));
 				$status		= 1;
 				$message	= 'Project was added successfully.';
 			} catch (Exception $e) {
 				$status		= 0;
 				$message	= $e->getMessage();
 			}
-			
+
 			echo json_encode(array('status' => $status, 'message' => $message));
 		}
 	}
-	
+
 	public function loadAction()
 	{
 		$projectId				= $this->_request->getParam('project');
 		$_SESSION['projectId']	= $projectId;
-		
+
 		$this->_forward('index', 'sdd', 'default');
 	}
 }
