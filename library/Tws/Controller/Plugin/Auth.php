48cb01c20dce024c6771f00140743666d2072d45
Chris Tankersley <dragonmantank@gmail.com>
2009-04-06 12:38:55 +0000

    Locked down Admin Module to only admins via the Auth plugin

--- library/Tws/Controller/Plugin/Auth.php
+++ library/Tws/Controller/Plugin/Auth.php
@@ -9,6 +9,7 @@ class Tws_Controller_Plugin_Auth extends Zend_Controller_Plugin_Abstract
 
 		$openModules	= array('auth', 'upload');
 
+		// Make sure that the user is logged in
 		if( !in_array($module, $openModules)) {
 			if( !$auth->hasIdentity()) {
 				$request->setModuleName('auth')
@@ -16,6 +17,21 @@ class Tws_Controller_Plugin_Auth extends Zend_Controller_Plugin_Abstract
 				   		->setActionName('login')
 			   			->setDispatched(true);
 			}
+		}
+
+		switch($module) {
+			// Only admins can get to the Admin section
+			case 'admin':
+				$user		= $auth->getIdentity();
+				$u			= new Users();
+
+				if( !($u->isAdmin($user->primaryGroup)) ) {
+					$request->setModuleName('default')
+							->setControllerName('restricted')
+							->setActionName('index')
+							->setDispatched(true);
+				}
+				break;
 		}
 	}
 }
