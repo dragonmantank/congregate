7cb95be82259c6e39b553eaf30888dad00d049d3
Chris Tankersley <dragonmantank@gmail.com>
2009-04-06 16:59:57 +0000

    File Revisions can now be uploaded

--- application/models/Files.php
+++ application/models/Files.php
@@ -47,6 +47,38 @@ class Files extends Zend_Db_Table_Abstract
 
 	}
 
+	public function addRevision($data)
+	{
+		$fileDetails	= new FilesDetail();
+		$file			= $this->fetchFile($data['fileId']);
+		$slug			= $file->directory;
+		$extPos			= strrpos($data['fileName'], '.');
+		$ext			= substr($data['fileName'], $extPos);
+		$fileName		= substr($data['fileName'], 0, $extPos);
+		$fsFilename		= $fileName . '_' . ($file->revision + 1) . $ext;
+
+		if(is_uploaded_file($data['tmp_name'])) {
+			$directory	= INSTALL_PATH . '/data/' . $slug;
+			if(!is_dir($directory)) {
+				mkdir($directory);
+			}
+
+			if(move_uploaded_file($data['tmp_name'], $directory . '/'. $fsFilename)) {
+				$detail['fileId']		= $data['fileId'];
+				$detail['fsFilename']	= $fsFilename;
+				$detail['mimetype']		= $data['mimetype'];
+				$detail['size']			= $data['size'];
+				$detail['author']		= $data['author'];
+
+				$detailId	= $fileDetails->insert($detail);
+
+				$this->update(array('detailId' => $detailId, 'revision' => ($file->revision +1)), 'id = ' . $data['fileId']);
+
+				return true;
+			}
+		}
+	}
+
 	public function addComment($fid, $author, $comment)
 	{
 		$file	= $this->fetchFile($fid);
@@ -79,9 +111,10 @@ class Files extends Zend_Db_Table_Abstract
 	public function fetchFile($fid)
 	{
 		$select	= $this->select()->from(array('f' => $this->_name))
-								 ->join(array('fd' => 'fd_FilesDetail'), 'fd.id = f.detailId')
+								 ->join(array('fd' => 'fd_FilesDetail'), 'fd.id = f.detailId', array('detailId' => 'id', 'fileId', 'fsFilename', 'mimetype', 'size', 'dateAdded', 'author'))
 								 ->join(array('u' => 'u_Users'), 'fd.author = u.id', array('name', 'email'))
 								 ->where('f.id = ?', $fid)
+								 ->order('fd.dateAdded DESC')
 								 ->setIntegrityCheck(false);
 
 		return $this->fetchRow($select);
@@ -90,7 +123,7 @@ class Files extends Zend_Db_Table_Abstract
 	public function fetchFilesByProject($pid)
 	{
 		$select	= $this->select()->from(array('f' => $this->_name))
-								 ->join(array('fd' => 'fd_FilesDetail'), 'fd.id = f.detailId')
+								 ->join(array('fd' => 'fd_FilesDetail'), 'fd.id = f.detailId', array('detailId' => 'id', 'fileId', 'fsFilename', 'mimetype', 'size', 'dateAdded', 'author'))
 								 ->join(array('u' => 'u_Users'), 'fd.author = u.id', array('name', 'email'))
 								 ->where('f.projectId = ?', $pid)
 								 ->setIntegrityCheck(false);
--- application/modules/default/controllers/FilesController.php
+++ application/modules/default/controllers/FilesController.php
@@ -13,6 +13,26 @@ class FilesController extends Zend_Controller_Action
 		echo json_encode(array('status' => 1, 'message' => 'Comment Added'));
 	}
 
+	public function addrevisionAction()
+	{
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(true);
+
+		$files	= new Files();
+		$data	= array(
+			'fileId'	=> $this->_request->getParam('fid'),
+			'tmp_name'	=> $_FILES['file']['tmp_name'],
+			'size'		=> $_FILES['file']['size'],
+			'mimetype'	=> $_FILES['file']['type'],
+			'fileName'	=> $_FILES['file']['name'],
+			'author'	=> Zend_Auth::getInstance()->getIdentity()->id,
+		);
+
+		$files->addRevision($data);
+		$this->_forward('stats', 'files', 'default', array('file' => $this->_request->getParam('fid')));
+
+	}
+
 	public function init()
 	{
 		$this->_helper->layout->setLayout('project-layout');
