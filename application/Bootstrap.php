1c348f85d2f37572c0b7cc3ac665741b8b269045
Chris Tankersley <dragonmantank@gmail.com>
2010-06-23 20:09:57 -0400

    Started working on layout, added Admin module

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,31 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    protected function _initAutoload()
+    {
+        $moduleLoader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => '',
+            'basePath'  => APPLICATION_PATH));
+        return $moduleLoader;
+    }
 
+    protected function _initFCPlugins()
+    {
+        $this->bootstrap('frontController');
+        $this->bootstrap('RegisterNamespaces');
 
+        $fc = $this->getResource('frontController');
+
+        // $fc->registerPlugin( new Manuscript_Controller_Plugin_Auth() );
+        $fc->registerPlugin( new Tws_Controller_Plugin_ModuleLayout() );
+
+        return $fc;
+    }
+
+    protected function _initRegisterNamespaces()
+    {
+        $loader = Zend_Loader_Autoloader::getInstance();
+        $loader->registerNamespace('Tws_');
+    }
 }
 
